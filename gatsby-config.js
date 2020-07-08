ab326351d843233afc3109614a57f8f95e643649
Sean Olson <seanton213@gmail.com>
2020-07-07 22:04:24 -0700

    Temp messaging

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-        title: `SeanTon`,
+        title: `Sean Olson Luna`,
         description: `Personal Website`,
         author: `Sean Olson Luna`,
     },
@@ -24,7 +24,7 @@ module.exports = {
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                icon: `src/images/blue-sun.png`, // This path is relative to the root of the site.
             },
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
